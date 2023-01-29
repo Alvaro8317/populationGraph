79f048c83b8de1ab75f8170dfaf86f729357d98a
Alvaro8317 <eduardo831_@hotmail.com>
2023-01-29 01:26:22 -0500

    feat: Add the option number 3, a chart with the percentage docs: Deleted some unnecesary print build: Created a function to show options at the moment of create a graph

--- helpers/read_data.py
+++ helpers/read_data.py
@@ -1,9 +1,23 @@
 import pandas as pd
-def read_data_from_csv(country = "Colombia", path = "data.csv",):
+def search_by_country(country, path):
     data = pd.read_csv(path)
     df = pd.DataFrame(data, columns=["Country/Territory", "2022 Population","2020 Population","2015 Population","2010 Population","2000 Population","1990 Population","1980 Population","1970 Population"])
     values = df[df["Country/Territory"] == country].values.tolist()[0]
     return dict(zip(df,values))
+
+
+def percentage_population(path):
+    data = pd.read_csv(path)
+    values = data.loc[:,"World Population Percentage"].values.tolist()
+    countries = data.loc[:,"Country/Territory"].values.tolist()
+    return dict(zip(countries,values))
+
+def read_data_from_csv(op, country = "Colombia", path = "data.csv"):
+    if op == 1:
+        return search_by_country(country, path)
+    elif op == 2:
+        return percentage_population(path)
+
 
 
 
--- main.py
+++ main.py
@@ -5,16 +5,28 @@ import os
 import calendar
 import json
 
+
 def write_log(details):
     with open("logs\log_error.log", "a", encoding="utf-8") as file:
         file.write(
             f"{datetime.fromtimestamp(calendar.timegm(time.gmtime()))}: {details}\n")
+
 
 def write_data(details, country):
     data = json.dumps(details, indent=4)
     with open(f"data\{date.today()} - {country}.json", "a", encoding="utf-8") as file:
         file.write(
             f"{data}\n")
+
+
+def ask_graph(result):
+    print(result)
+    form = input(
+        "Do you want a pie or a bar graph? Please write bar or pie\n").lower()
+    if form in ["bar", "pie"]:
+        create_graph(result, form)
+    else:
+        print("Invalid option")
 
 
 if __name__ == "__main__":
@@ -26,28 +38,27 @@ if __name__ == "__main__":
             Please introduce an option: 
             1. Initialize the application (Recommended for the first start)
             2. Search by country the population
+            3. Make a chart with the percentage of the population by country
             0. Exit
             """))
             match dec:
                 case 1:
                     create_folder(path)
                 case 2:
-                    country = input("Please introduce the name of a country: ").capitalize()
+                    country = input(
+                        "Please introduce the name of a country: ").title()
+                    print(country)
                     try:
-                        result = read_data(country)
+                        result = read_data(1, country)
                         try:
-                            write_data(result,country)
-                            print(f"Information saved successfully in the folder data")
+                            write_data(result, country)
+                            print(
+                                f"Information saved successfully in the folder data")
                             graph = input(
                                 "Do you want to graph the data? Please write Yes or No\n").lower()
                             match graph:
                                 case "yes":
-                                    form = input(
-                                        "Do you want a pie or a bar graph? Please write bar or pie\n").lower()
-                                    if form in ["bar", "pie"]:
-                                        create_graph(result, form)
-                                    else:
-                                        print("Invalid option")
+                                    ask_graph(result)
                                 case "no":
                                     pass
                         except Exception as error:
@@ -56,6 +67,10 @@ if __name__ == "__main__":
                     except IndexError as err:
                         write_log(err)
                         print("Invalid country, check the log")
+                case 3:
+                    data = read_data(2)
+                    print(data)
+                    ask_graph(data)
                 case 0:
                     break
                 case _:
